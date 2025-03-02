ed289f019a1184d9cbf79ffafe1a7cd86a3d371f
Nazem <Nazem@DESKTOP-ACADKUR>
2025-03-02 13:10:55 +0330

    signupchange

--- DRR.Application/CommandHandlers/Authentication/SignUpCommandHandler.cs
+++ DRR.Application/CommandHandlers/Authentication/SignUpCommandHandler.cs
@@ -177,124 +177,187 @@ namespace DRR.Application.CommandHandlers.Authentication
                 throw new UserNameOrPhoneNumberIsNotValidException();
             if (!command.UserName.IsNationalCodeValid())
                 throw new IdentityException("شماره ملی معتبر نمیباشد .", "NationalCode is not valid .");
-
-
-            var user = new ApplicationUser
+            ApplicationUser user;
+            if (_userManager.FindByNameAsync(command.UserName).Result != null)
             {
-                UserName = command.UserName,
-                PhoneNumber = command.PhoneNumber.ConvertToValidMobile(),
-                Fullname = command.Fullname
-            };
-
-            var userCreate = await _userManager.CreateAsync(user, command.Password);
-            if (userCreate.Succeeded)
-            {
-                if (command.UserName.IsNullOrEmptyExtension())
-                    throw new UserNameAndPasswordAreNullException();
-
-                //////////////////////////////////////////////////sms
-                var activationCode = new Random().Next(10000, 99999).ToString();
-                var message = _smsSetting.ActivatingRegistrationMessage.Replace("{activationCode}", activationCode);
-                var receiverMobile = command.PhoneNumber.RemoveMobilePrefix();
-
-                var smsAclOutputModel = await _smsAcl.Send(new SmsAclInputModel { Message = message, Receiver = receiverMobile });
-                if (!smsAclOutputModel.IsSuccess)
-                    throw new ActivatingCodeNotSendedException();
-
-                var smsInfo = new SmsInfo(_smsSetting.Sender, receiverMobile.ConvertToValidMobile(), message, activationCode,
-                    SmsType.ActivatingRegistration);
-
-                await _smsInfoRepository.Create(smsInfo);
-                /////////////
-
-
-                if (command.UserName.Length == 10)
+                user = _userManager.FindByNameAsync(command.UserName).Result;
+                if (user.PhoneNumberConfirmed != true)
                 {
-                    var userExist = await _userManager.Users.FirstOrDefaultAsync(u => u.NormalizedUserName == command.UserName.ToUpper());
-                    var tokenHandler = new JwtSecurityTokenHandler();
-                    var key = Encoding.ASCII.GetBytes("@#MY_BIG_SECRET_KEYMY_BIG_SECRET_KEYMY_BIG_SECRET_KEYMY_BIG_SECRET_KEY@#");
-                    var tokenDescription = new SecurityTokenDescriptor
+                    user.PhoneNumber = command.PhoneNumber.ConvertToValidMobile();
+                    user.Fullname = command.Fullname;
+                    await _userManager.UpdateAsync(user);
+                    var activationCode = new Random().Next(10000, 99999).ToString();
+                    var message = _smsSetting.ActivatingRegistrationMessage.Replace("{activationCode}", activationCode);
+                    var receiverMobile = command.PhoneNumber.RemoveMobilePrefix();
+
+                    var smsAclOutputModel = await _smsAcl.Send(new SmsAclInputModel { Message = message, Receiver = receiverMobile });
+                    if (!smsAclOutputModel.IsSuccess)
+                        throw new ActivatingCodeNotSendedException();
+
+                    var smsInfo = new SmsInfo(_smsSetting.Sender, receiverMobile.ConvertToValidMobile(), message, activationCode,
+                        SmsType.ActivatingRegistration);
+
+                    await _smsInfoRepository.Create(smsInfo);
+                    /////////////
+
+
+                    if (command.UserName.Length == 10)
                     {
-                        Subject = new ClaimsIdentity(new Claim[]
-                            {
+                        var userExist = await _userManager.Users.FirstOrDefaultAsync(u => u.NormalizedUserName == command.UserName.ToUpper());
+                        var tokenHandler = new JwtSecurityTokenHandler();
+                        var key = Encoding.ASCII.GetBytes("@#MY_BIG_SECRET_KEYMY_BIG_SECRET_KEYMY_BIG_SECRET_KEYMY_BIG_SECRET_KEY@#");
+                        var tokenDescription = new SecurityTokenDescriptor
+                        {
+                            Subject = new ClaimsIdentity(new Claim[]
+                                {
                             new Claim(ClaimTypes.Name, userExist.Id)
-                            }
-                        ),
-                        Expires = DateTime.UtcNow.AddDays(Convert.ToInt32(JwtSetting.Time)),
-                        SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key),
-                            SecurityAlgorithms.HmacSha256Signature),
-                    };
-                    var token = tokenHandler.CreateToken(tokenDescription);
-                    var tokenString = tokenHandler.WriteToken(token);
+                                }
+                            ),
+                            Expires = DateTime.UtcNow.AddDays(Convert.ToInt32(JwtSetting.Time)),
+                            SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key),
+                                SecurityAlgorithms.HmacSha256Signature),
+                        };
+                        var token = tokenHandler.CreateToken(tokenDescription);
+                        var tokenString = tokenHandler.WriteToken(token);
 
-                    await _userManager.RemoveAuthenticationTokenAsync(userExist, userExist.Id, tokenString);
-                    var newRefreshToken = await _userManager.GenerateUserTokenAsync(userExist, "Default", tokenString);
-                    await _userManager.SetAuthenticationTokenAsync(userExist, userExist.UserName, "Authorization", tokenString);
+                        await _userManager.RemoveAuthenticationTokenAsync(userExist, userExist.Id, tokenString);
+                        var newRefreshToken = await _userManager.GenerateUserTokenAsync(userExist, "Default", tokenString);
+                        await _userManager.SetAuthenticationTokenAsync(userExist, userExist.UserName, "Authorization", tokenString);
 
-                    return new SignUpCommandResponse()
-                    {
-                        Token = $"Bearer {tokenString}",
-                        ExpiredAt = JwtSetting.Time,
-                        RefreshToken = $"Bearer {tokenString}",
-                        UserFullname = userExist.Fullname ?? "",
-                        Code = 1,
-                        Message = "User Has Been Created"
-                    };
+                        return new SignUpCommandResponse()
+                        {
+                            Token = $"Bearer {tokenString}",
+                            ExpiredAt = JwtSetting.Time,
+                            RefreshToken = $"Bearer {tokenString}",
+                            UserFullname = userExist.Fullname ?? "",
+                            Code = 1,
+                            Message = "User Has Been Created"
+                        };
+                    }
                 }
-                //TODO:FindByPhoneNumber
-                else
+            }
+            else
+            {
+                user = new ApplicationUser
                 {
-                    if (command.UserName.Length != 13)
+                    UserName = command.UserName,
+                    PhoneNumber = command.PhoneNumber.ConvertToValidMobile(),
+                    Fullname = command.Fullname
+                };
+                var userCreate = await _userManager.CreateAsync(user, command.Password);
+                if (userCreate.Succeeded)
+                {
+                    if (command.UserName.IsNullOrEmptyExtension())
+                        throw new UserNameAndPasswordAreNullException();
+
+                    //////////////////////////////////////////////////sms
+                    var activationCode = new Random().Next(10000, 99999).ToString();
+                    var message = _smsSetting.ActivatingRegistrationMessage.Replace("{activationCode}", activationCode);
+                    var receiverMobile = command.PhoneNumber.RemoveMobilePrefix();
+
+                    var smsAclOutputModel = await _smsAcl.Send(new SmsAclInputModel { Message = message, Receiver = receiverMobile });
+                    if (!smsAclOutputModel.IsSuccess)
+                        throw new ActivatingCodeNotSendedException();
+
+                    var smsInfo = new SmsInfo(_smsSetting.Sender, receiverMobile.ConvertToValidMobile(), message, activationCode,
+                        SmsType.ActivatingRegistration);
+
+                    await _smsInfoRepository.Create(smsInfo);
+                    /////////////
+
+
+                    if (command.UserName.Length == 10)
                     {
-                        command.UserName = command.UserName.ConvertToValidMobile();
+                        var userExist = await _userManager.Users.FirstOrDefaultAsync(u => u.NormalizedUserName == command.UserName.ToUpper());
+                        var tokenHandler = new JwtSecurityTokenHandler();
+                        var key = Encoding.ASCII.GetBytes("@#MY_BIG_SECRET_KEYMY_BIG_SECRET_KEYMY_BIG_SECRET_KEYMY_BIG_SECRET_KEY@#");
+                        var tokenDescription = new SecurityTokenDescriptor
+                        {
+                            Subject = new ClaimsIdentity(new Claim[]
+                                {
+                            new Claim(ClaimTypes.Name, userExist.Id)
+                                }
+                            ),
+                            Expires = DateTime.UtcNow.AddDays(Convert.ToInt32(JwtSetting.Time)),
+                            SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key),
+                                SecurityAlgorithms.HmacSha256Signature),
+                        };
+                        var token = tokenHandler.CreateToken(tokenDescription);
+                        var tokenString = tokenHandler.WriteToken(token);
+
+                        await _userManager.RemoveAuthenticationTokenAsync(userExist, userExist.Id, tokenString);
+                        var newRefreshToken = await _userManager.GenerateUserTokenAsync(userExist, "Default", tokenString);
+                        await _userManager.SetAuthenticationTokenAsync(userExist, userExist.UserName, "Authorization", tokenString);
+
+                        return new SignUpCommandResponse()
+                        {
+                            Token = $"Bearer {tokenString}",
+                            ExpiredAt = JwtSetting.Time,
+                            RefreshToken = $"Bearer {tokenString}",
+                            UserFullname = userExist.Fullname ?? "",
+                            Code = 1,
+                            Message = "User Has Been Created"
+                        };
+                    }
+                    //TODO:FindByPhoneNumber
+                    else
+                    {
+                        if (command.UserName.Length != 13)
+                        {
+                            command.UserName = command.UserName.ConvertToValidMobile();
+                        }
+
+                        var userExist = await _userManager.Users.FirstOrDefaultAsync(l => l.PhoneNumber == command.UserName);
+
+                        if (userExist == null)
+                            throw new UserNotExistException();
+
+                        var signOn = await _signInManager.PasswordSignInAsync(userExist.UserName, command.Password, false, false);
+
+                        if (!signOn.Succeeded) throw new IdentityUsernameOrPasswordException();
+
+
+                        var tokenHandler = new JwtSecurityTokenHandler();
+                        var key = Encoding.ASCII.GetBytes("@#MY_BIG_SECRET_KEYMY_BIG_SECRET_KEYMY_BIG_SECRET_KEYMY_BIG_SECRET_KEY@#");
+                        var tokenDescription = new SecurityTokenDescriptor
+                        {
+                            Subject = new ClaimsIdentity(new Claim[]
+                                {
+                            new Claim(ClaimTypes.Name, userExist.Id)
+                                }
+                            ),
+                            Expires = DateTime.UtcNow.AddDays(Convert.ToInt32(JwtSetting.Time)),
+                            SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key),
+                                SecurityAlgorithms.HmacSha256Signature),
+                        };
+                        var token = tokenHandler.CreateToken(tokenDescription);
+                        var tokenString = tokenHandler.WriteToken(token);
+
+
+                        await _userManager.RemoveAuthenticationTokenAsync(userExist, userExist.Id, tokenString);
+                        var newRefreshToken = await _userManager.GenerateUserTokenAsync(userExist, "Default", tokenString);
+                        await _userManager.SetAuthenticationTokenAsync(userExist, userExist.UserName, "Authorization", tokenString);
+
+                        return new SignUpCommandResponse()
+                        {
+                            Token = $"Bearer {tokenString}",
+                            ExpiredAt = JwtSetting.Time,
+                            RefreshToken = $"Bearer {tokenString}",
+                            Code = 1,
+                            Message = "User Has Been Created"
+                        };
                     }
 
-                    var userExist = await _userManager.Users.FirstOrDefaultAsync(l => l.PhoneNumber == command.UserName);
-
-                    if (userExist == null)
-                        throw new UserNotExistException();
-
-                    var signOn = await _signInManager.PasswordSignInAsync(userExist.UserName, command.Password, false, false);
-
-                    if (!signOn.Succeeded) throw new IdentityUsernameOrPasswordException();
 
 
-                    var tokenHandler = new JwtSecurityTokenHandler();
-                    var key = Encoding.ASCII.GetBytes("@#MY_BIG_SECRET_KEYMY_BIG_SECRET_KEYMY_BIG_SECRET_KEYMY_BIG_SECRET_KEY@#");
-                    var tokenDescription = new SecurityTokenDescriptor
-                    {
-                        Subject = new ClaimsIdentity(new Claim[]
-                            {
-                            new Claim(ClaimTypes.Name, userExist.Id)
-                            }
-                        ),
-                        Expires = DateTime.UtcNow.AddDays(Convert.ToInt32(JwtSetting.Time)),
-                        SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key),
-                            SecurityAlgorithms.HmacSha256Signature),
-                    };
-                    var token = tokenHandler.CreateToken(tokenDescription);
-                    var tokenString = tokenHandler.WriteToken(token);
-
-
-                    await _userManager.RemoveAuthenticationTokenAsync(userExist, userExist.Id, tokenString);
-                    var newRefreshToken = await _userManager.GenerateUserTokenAsync(userExist, "Default", tokenString);
-                    await _userManager.SetAuthenticationTokenAsync(userExist, userExist.UserName, "Authorization", tokenString);
-
-                    return new SignUpCommandResponse()
-                    {
-                        Token = $"Bearer {tokenString}",
-                        ExpiredAt = JwtSetting.Time,
-                        RefreshToken = $"Bearer {tokenString}",
-                        Code = 1,
-                        Message = "User Has Been Created"
-                    };
                 }
 
-
-
+                throw new IdentityException(null, userCreate.Errors.First().Description);
             }
 
-            throw new IdentityException(null, userCreate.Errors.First().Description);
+
+            return Ok();
+
         }
     }
 }
