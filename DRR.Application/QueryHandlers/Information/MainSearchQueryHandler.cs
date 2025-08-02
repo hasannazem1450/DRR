783abfa7d62aa0ca926d46defa4305236bacd49c
hasan nazem <hasan nazem@DESKTOP-R1KQBQ9>
2025-08-02 22:38:18 +0330

    pinglish search is ok inthis version

--- DRR.Application/QueryHandlers/Information/MainSearchQueryHandler.cs
+++ DRR.Application/QueryHandlers/Information/MainSearchQueryHandler.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using DRR.Application.Contracts.Commands.Information;
@@ -12,8 +13,10 @@ using DRR.Application.Contracts.Repository.Information;
 using DRR.Application.Contracts.Repository.Insurance;
 using DRR.Application.Contracts.Repository.Specialists;
 using DRR.Application.Contracts.Repository.TreatmentCenters;
+using DRR.CommandDb.Migrations;
 using DRR.Framework.Contracts.Markers;
 using DRR.Utilities.Extensions;
+using static System.Net.Mime.MediaTypeNames;
 
 
 namespace DRR.Application.QueryHandlers.Information
@@ -41,16 +44,128 @@ namespace DRR.Application.QueryHandlers.Information
             _doctorTreatmentCenterRepository = doctorTreatmentCenterRepository;
             _searchHistoryRepository = searchHistoryRepository;
         }
+        static string EnglishToPersian(string text)
+        {
+            // Define a mapping from English letters to Persian letters
+            var map = new System.Collections.Generic.Dictionary<char, char>
+        {
+            { 'p', 'ح' },  
+            { 'o', 'خ' },  
+            { 'i', 'ه' },  
+            { 'u', 'ع' },  
+            { 'y', 'غ' },  
+            { 't', 'ف' },  
+            { 'r', 'ق' },  
+            { 'e', 'ث' },  
+            { 'w', 'ص' },  
+            { 'q', 'ض' },  
+            { 'a', 'ش' },  
+            { 's', 'س' },  
+            { 'd', 'ی' },  
+            { 'f', 'ب' },  
+            { 'g', 'ل' },  
+            { 'h', 'ا' },  
+            { 'j', 'ت' },  
+            { 'k', 'ن' },  
+            { 'l', 'م' },  
+            { 'z', 'ظ' },  
+            { 'x', 'ط' },  
+            { 'c', 'ز' },  
+            { 'v', 'ر' },  
+            { 'b', 'ذ' },  
+            { 'n', 'د' },  
+            { 'm', 'ئ' },  
+            { ',', 'و' },
+            
+          
+            //{ 'p', 'ح' },  
+            //{ 'O', 'خ' },  
+            //{ 'I', 'ه' },  
+            //{ 'U', 'ع' },  
+            //{ 'Y', 'غ' },  
+            //{ 'T', 'ف' },  
+            //{ 'R', 'ق' },  
+            //{ 'E', 'ث' },  
+            //{ 'W', 'ص' },  
+            //{ 'Q', 'ض' },  
+            //{ 'A', 'ش' },  
+            //{ 'S', 'س' },  
+            //{ 'D', 'ی' },  
+            //{ 'F', 'ب' },  
+            //{ 'G', 'ل' },  
+            //{ 'H', 'ا' },  
+            //{ 'J', 'ت' },  
+            //{ 'K', 'ن' },  
+            //{ 'L', 'م' },  
+            //{ 'Z', 'ظ' },  
+            //{ 'X', 'ط' },  
+            //{ 'C', 'ز' },  
+            //{ 'V', 'ر' },  
+            //{ 'B', 'ذ' },  
+            //{ 'N', 'د' },  
+            //{ 'M', 'ئ' },  
 
+            // add more mappings if you want
+        };
+
+            var result = new StringBuilder();
+
+            foreach (char c in text)
+            {
+                // If lowercase version exists in map, replace it
+                if (map.TryGetValue(char.ToLower(c), out char persianChar))
+                {
+                    result.Append(persianChar);
+                }
+                else
+                {
+                    // Otherwise, keep the original character
+                    result.Append(c);
+                }
+            }
+
+            return result.ToString();
+        }
         public async Task<MainSearchQueryResponse> Execute(MainSearchQuery query,
         CancellationToken cancellationToken)
         {
-
+           
             if (query.Term.IsNullOrEmptyExtension() || query.Term.Length <= 2)
             {
                 List<String> list = new List<string>();
                 return
                     new MainSearchQueryResponse { Suggest = await _searchHistoryRepository.ReadSuggestionByTerm(list) };
+            }
+            //this code adde by nazem to check and help if the user dont have persian key board installed
+
+            string returenedchoise = "";
+            bool hasEnglish = false;
+            bool hasPersian = false;
+
+            foreach (char c in query.Term)
+            {
+                if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+                {
+                    hasEnglish = true;
+                }
+                else if ((c >= '\u0600' && c <= '\u06FF') || (c >= '\u0750' && c <= '\u077F'))
+                {
+                    hasPersian = true;
+                }
+            }
+
+            if (hasPersian && !hasEnglish)
+                returenedchoise = "Persian";
+            else if (hasEnglish && !hasPersian)
+                returenedchoise = "English";
+            else if (hasEnglish && hasPersian)
+                returenedchoise = "Mixed";
+            else
+                returenedchoise = "Unknown";
+
+            if (returenedchoise == "English")
+            {
+                query.Term = EnglishToPersian(query.Term);
             }
             var searchTerms = query.Term.Split(" ", StringSplitOptions.RemoveEmptyEntries).ToList();
             string suggest = await _searchHistoryRepository.ReadSuggestionByTerm(searchTerms);
